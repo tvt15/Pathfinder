3ae253e768e6dcdfc54bbe75c43a4d8aef1be6b8
tvt15 <73994466+tvt15@users.noreply.github.com>
2021-08-17 20:19:31 +0530

    Add files via upload

---
+++ HMPS_PROJECT.py
@@ -0,0 +1,26 @@
+import pygame
+import math 
+from queue import PriorityQueue
+
+WIDTH = 800
+WINDOW = pygame.display.set_mode((WIDTH,WIDTH))
+pygame.display.set_caption("HMPS PROJECT:PATHFINDER")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+class Spot:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row*width
+        self.y = col*width
+        
